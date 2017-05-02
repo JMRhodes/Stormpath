d51e2c3b6ff260fdaf9271107bf1a0edc98ff18e
Justin Rhodes <justin.m.rhodes@gmail.com>
2017-05-01 20:20:30 -0500

    Update login styles

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -74,12 +74,12 @@ class ProfileController extends Controller {
     }
 
     public static function getUserAvatar( $user_id ) {
-        $file = FileEntry::where( 'user_id', $user_id )->get()->last();
+        $file   = FileEntry::where( 'user_id', $user_id )->get()->last();
         $exists = Storage::disk( 'uploads' )->exists( 'avatars/' . $file->filename );
         if ( $file->filename && $exists ) {
             return url( 'uploads/avatars/' . $file->filename );
         }
 
-        return url('/images/user.svg');
+        return url( '/images/user.svg' );
     }
 }
